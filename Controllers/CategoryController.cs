96bed3bf5e3b848abbe79bdc936dac91bb83c576
Mateus Nadal <nadalmateus@hotmail.com>
2022-06-16 20:32:38 -0300

    feat: added first controller and status chech controller

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,18 @@
+﻿using Blog.Data;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet("")]
+        public IActionResult Get([FromServices] BlogDataContext context)
+        {
+            var categories = context.Categories.ToList();
+            return Ok(categories);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,15 @@
+using Blog.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<BlogDataContext>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,9 +17,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
