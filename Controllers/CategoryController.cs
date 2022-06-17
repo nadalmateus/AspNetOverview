2d7782fa1dca32abfcf104b2f37e0292f21f16c8
Mateus Nadal <nadalmateus@hotmail.com>
2022-06-16 22:09:41 -0300

    feat: added first crud, and errors validations

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using Blog.Data;
+using Blog.Models;
 
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -11,8 +12,116 @@ namespace Blog.Controllers
         [HttpGet("v1/categories")]
         public async Task<IActionResult> GetAsync([FromServices] BlogDataContext context)
         {
-            var categories = await context.Categories.ToListAsync();
-            return Ok(categories);
+            try
+            {
+                var categories = await context.Categories.ToListAsync();
+                return Ok(categories);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "05X10 - Falha inteterna no servidor");
+            }
+        }
+
+        [HttpGet("v1/categories/{id:int}")]
+        public async Task<IActionResult> GetByIdAsync([FromRoute] int id, [FromServices] BlogDataContext context)
+        {
+            try
+            {
+                var category = await context.Categories.FirstOrDefaultAsync(i => i.Id == id);
+
+                if (category == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(category);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "05X10 - Falha inteterna no servidor");
+            }
+        }
+
+        [HttpPost("v1/categories")]
+        public async Task<IActionResult> PostAsync([FromBody] Category categoryFromBody, [FromServices] BlogDataContext context)
+        {
+            try
+            {
+                await context.Categories.AddAsync(categoryFromBody);
+                await context.SaveChangesAsync();
+
+                return Created($"v1/categories/{categoryFromBody.Id}", categoryFromBody);
+            }
+            catch (DbUpdateException ex)
+            {
+                return StatusCode(500, "05XE9 - Não foi possivel incluir a categoria no banco");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "05X10 - Falha inteterna no servidor");
+            }
+        }
+
+        [HttpPut("v1/categories/{id:int}")]
+        public async Task<IActionResult> PutAsync([FromRoute] int id, [FromBody] Category categoryFromBody, [FromServices] BlogDataContext context)
+        {
+            try
+            {
+                var category = await context.Categories.FirstOrDefaultAsync(i => i.Id == id);
+
+                if (category == null)
+                {
+                    return NotFound();
+                }
+
+                category.Name = categoryFromBody.Name;
+                category.Slug = categoryFromBody.Slug;
+
+                context.Categories.Update(category);
+                await context.SaveChangesAsync();
+
+                return Ok(categoryFromBody);
+            }
+            catch (DbUpdateException ex)
+            {
+                return StatusCode(500, "05XE8 - Não foi possivel alterar a categoria no banco");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "05X11 - Falha inteterna no servidor");
+            }
+        }
+
+        [HttpDelete("v1/categories/{id:int}")]
+        public async Task<IActionResult> DeleteAsync([FromRoute] int id, [FromServices] BlogDataContext context)
+        {
+            try
+            {
+                var category = await context.Categories.FirstOrDefaultAsync(i => i.Id == id);
+
+                if (category == null)
+                {
+                    return NotFound();
+                }
+
+                context.Categories.Remove(category);
+                await context.SaveChangesAsync();
+
+                return Ok(category);
+            }
+            catch (DbUpdateException ex)
+            {
+                return StatusCode(500, "05XE7 - Não foi possivel excluir a categoria no banco");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "05X12 - Falha inteterna no servidor");
+            }
         }
     }
 }
+
+
+
+
