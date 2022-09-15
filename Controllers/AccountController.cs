3d19810934483e0912078aa0d28f1ef004225608
Mateus Nadal <nadalmateus@hotmail.com>
2022-09-15 19:31:54 -0300

    style: dotnet format

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -15,4 +15,3 @@ public class AccountController : ControllerBase
         return Ok(token);
     }
 }
-
--- Program.cs
+++ Program.cs
@@ -10,8 +10,7 @@ using Microsoft.IdentityModel.Tokens;
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 
-builder.Services.AddControllers()
-    .ConfigureApiBehaviorOptions(options => options.SuppressModelStateInvalidFilter = true);
+builder.Services.AddControllers().ConfigureApiBehaviorOptions(options => options.SuppressModelStateInvalidFilter = true);
 builder.Services.AddDbContext<BlogDataContext>();
 builder.Services.AddTransient<TokenService>();
 builder.Services.AddEndpointsApiExplorer();
